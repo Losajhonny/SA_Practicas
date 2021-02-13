0e566b31ebf7a4975348980e24d532284ce8bb94
Jhonny <2564550230101@ingeniera.usac.edu.gt>
2021-02-12 22:28:26 -0600

    readme.md

---
+++ Practica2/Parte1/README.md
@@ -0,0 +1,7 @@
+![logo](../../doc/Logo.png)
+
+![menu](../../doc/p2menu.png)
+
+![gentok](../../doc/p2gentok.png)
+
+![valtok](../../doc/p2valtok.png)
--- Practica2/Parte1/app.py
+++ Practica2/Parte1/app.py
@@ -1,14 +1,19 @@
+# librerias
 import uuid
 import jwt
 
+# constantes
 TOKENS = []
 SECRET = []
 
+# generador tokens con payload 'carnet', 'nombre
 def gen():
     print("-- generar token --")
+    # obtener datos
     carnet = input("Carnet: ")
     name = input("Nombre: ")
 
+    # encriptacion con jwt
     secret = str(uuid.uuid4()).replace("-", "")
     encoded = jwt.encode({ "carnet": carnet, "name": name }, secret, algorithm="HS256")
 
@@ -16,35 +21,37 @@ def gen():
     print("Token:", encoded)
     print("")
 
+    # almacenar datos jwt
     TOKENS.append(encoded)
     SECRET.append(secret)
 
+# decodifica tokens que estan en memoria
 def decode():
     print("-- validar token --")
 
+    # mostrar lista tokens
     for i in range(0, len(TOKENS)):
         print(str(i) + ".", "Secret:", SECRET[i], "Token:", TOKENS[i])
 
     print("")
     x = int(input("seleccione un token: "))
 
+    # obtner datos de jwt
     secret = SECRET[x]
     token = TOKENS[x]
 
+    # decodificar y volver a codificar
     dec = jwt.decode(token, secret, algorithms="HS256")
     enc = jwt.encode(dec, secret, algorithm="HS256")
 
+    # validacion token
     if token == enc:
         print("TOKEN VALIDO")
         print("")
-    
-    SECRET.pop(x)
-    TOKENS.pop(x)
-    
 
+# funcion principal -- menu --
 def main():
     while True:
-
         print("-------------- Generador token ---------------")
         print("1. Generar token")
         print("2. Decodificar token")
