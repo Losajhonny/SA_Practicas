069cf12f80b2314368d03c6ce943ff2edf0d857e
Jhonny <2564550230101@ingeniera.usac.edu.gt>
2021-02-12 22:26:55 -0600

    readme.md

---
+++ Practica2/Parte2/README.md
@@ -0,0 +1,7 @@
+![logo](../../doc/Logo.png)
+
+![menu](../../doc/p2menucal.png)
+
+![add](../../doc/p2addcal.png)
+
+![div](../../doc/p2divcal.png)
--- Practica2/Parte2/app.py
+++ Practica2/Parte2/app.py
@@ -1,9 +1,10 @@
+# librerias
 from bs4 import BeautifulSoup
 import requests
 
+# constantes
 URL = "http://www.dneonline.com/calculator.asmx"
 HEADERS = { 'content-type': 'application/soap+xml' }
-
 INI = """<?xml version="1.0" encoding="utf-8"?>
         <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
         <soap12:Body>"""
@@ -11,9 +12,10 @@ INI = """<?xml version="1.0" encoding="utf-8"?>
 FIN = """</soap12:Body>
         </soap12:Envelope>"""
 
+# realiza una suma a + b
 def add(a, b):
     body = INI + """<Add xmlns="http://tempuri.org/"><intA>""" + a + """</intA><intB>""" + b + """</intB></Add>""" + FIN
-    
+    # peticion soap
     res = requests.post(URL,data=body,headers=HEADERS)
     res = BeautifulSoup(res.content, 'lxml')
     res = res.find('addresult').text
@@ -21,12 +23,14 @@ def add(a, b):
     print(a + " + " + b + " = " + res)
     print("")
 
+# realiza una division a / b
 def divide(a, b):
     body = INI + """<Divide xmlns="http://tempuri.org/"><intA>""" + a + """</intA><intB>""" + b + """</intB></Divide>""" + FIN
-    
+    # peticion soap
     res = requests.post(URL,data=body,headers=HEADERS)
     res = BeautifulSoup(res.content, 'lxml')
 
+    # validacion
     if int(b) == 0:
         res = "error"
     else:
@@ -35,9 +39,10 @@ def divide(a, b):
     print(a + " / " + b + " = " + res)
     print("")
 
+# realiza una multiplicacion a * b
 def multiply(a, b):
     body = INI + """<Multiply xmlns="http://tempuri.org/"><intA>""" + a + """</intA><intB>""" + b + """</intB></Multiply>""" + FIN
-    
+    # peticion soap
     res = requests.post(URL,data=body,headers=HEADERS)
     res = BeautifulSoup(res.content, 'lxml')
     res = res.find('multiplyresult').text
@@ -45,9 +50,10 @@ def multiply(a, b):
     print(a + " * " + b + " = " + res)
     print("")
 
+# realiza una resta a - b
 def substract(a, b):
     body = INI + """<Subtract xmlns="http://tempuri.org/"><intA>""" + a + """</intA><intB>""" + b + """</intB></Subtract>""" + FIN
-    
+    # peticion soap
     res = requests.post(URL,data=body,headers=HEADERS)
     res = BeautifulSoup(res.content, 'lxml')
     res = res.find('subtractresult').text
@@ -55,6 +61,7 @@ def substract(a, b):
     print(a + " - " + b + " = " + res)
     print("")
 
+# menu principal
 def main():
     while True:
         print("---------- Calculator ----------")
